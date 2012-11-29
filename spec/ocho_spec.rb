be99a00beacd2472d08dbf5de386f00233e254f5
andrea longhi <andrea@spaghetticode.it>
2012-11-29 14:30:05 +0100

    add Ocho module specs

--- spec/ocho_spec.rb
+++ spec/ocho_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe Ocho do
+  describe '::encoding' do
+    it 'returns the encoding of a string' do
+      Ocho.encoding('runaway').should == 'ascii'
+    end
+  end
+
+  describe '::read' do
+    it 'reads a file and converts the content to ascii' do
+      text = Ocho.read fixture_file('EUC-KR.txt')
+      Ocho.encoding(text).should == 'ascii'
+    end
+  end
 end
