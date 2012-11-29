1d0cd3320569b7f108791418a1a6aec51a1d9dd8
andrea longhi <andrea@spaghetticode.it>
2012-11-29 14:32:48 +0100

    add converter code

--- lib/ocho/converter.rb
+++ lib/ocho/converter.rb
@@ -1,6 +1,30 @@
 require 'iconv'
 require 'rchardet'
+require 'active_support/core_ext/class/attribute'
 
 module Ocho
+  class Converter
+    class_attribute :strategy
 
+    class << self
+      def strategy
+        '//TRANSLIT//IGNORE'
+      end
+    end
+
+    attr_reader :text
+    attr_writer :strategy
+
+    def initialize(text)
+      @text = text
+    end
+
+    def encoding
+      CharDet.detect(text)['encoding']
+    end
+
+    def to_utf8
+      Iconv.conv("#{encoding}#{strategy}", 'utf-8', text)
+    end
+  end
 end
