824d1ba5de7b213183fd4e610b800c68be4a2890
andrea longhi <andrea@spaghetticode.it>
2012-11-29 14:32:57 +0100

    add ocho module code

--- lib/ocho.rb
+++ lib/ocho.rb
@@ -2,4 +2,14 @@ require 'ocho/version'
 
 module Ocho
   autoload :Converter, 'ocho/converter'
+  extend self
+
+  def encoding(text)
+    Converter.new(text).encoding
+  end
+
+  def read(filename)
+    content = File.read(filename)
+    Converter.new(content).to_utf8
+  end
 end
